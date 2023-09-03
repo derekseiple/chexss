099a2c60cee29fdafceefc659e7093fe20854f71
Derek Seiple <derekseiple@gmail.com>
2023-09-03 16:08:52 -0400

    Adds board drawer

---
+++ scripts/image_generator/src/board_drawer.py
@@ -0,0 +1,51 @@
+"""
+board_drawer.py
+Copyright © 2023 Derek Seiple
+Licensed under Creative Commons BY-NC-SA 3.0. See license file.
+"""
+from PIL import Image
+from typing import Callable, List
+from src.hex_meta import HexMeta
+from src.board_image import BoardImage
+from src.board_coordinates import BoardCoordinate
+from src.drawer_utils import board_coordinate_image_location
+
+
+class BoardDrawer:
+    """This class is constructed with a BoardImage which is then used as a base to draw other images on top of it. The
+    draw method below will then draw images on top of the board image at the given coordinates.
+    """
+    
+    def __init__(
+        self,
+        board_image: BoardImage,
+    ) -> None:
+        self._board_image = board_image
+        self._image = self._board_image.image.copy()
+
+    def draw(
+        self,
+        image_factory: Callable[[int, BoardCoordinate, HexMeta], Image.Image],
+        coordinates: List[BoardCoordinate]
+    ) -> None:
+        """For each coordinate in the list, draw the image returned by the image factory at the coordinate on the board.
+
+        Parameters
+        ----------
+        image_factory: Callable[[int, BoardCoordinate, HexMeta], Image.Image]
+            A function that takes 1) the dimension of the board, 2) the coordinate on the board where the image will go,
+            and 3) the hex metadata that describes the hexes on the board. The function should return a Pillow image of
+            the image to draw at the coordinate.
+        
+        coordinates: List[BoardCoordinate]
+            The coordinates on the board to draw the images at with the image factory.
+        """
+        for coordinate in coordinates:
+            self._image.alpha_composite(
+                image_factory(self._board_image.dimension, coordinate, self._board_image.hex_meta),
+                board_coordinate_image_location(self._board_image.dimension, coordinate, self._board_image.hex_meta)
+            )
+
+    @property
+    def image(self) -> Image.Image:
+        return self._image
---
+++ scripts/image_generator/src/drawer_utils.py
@@ -0,0 +1,42 @@
+"""
+drawer_utils.py
+Copyright © 2023 Derek Seiple
+Licensed under Creative Commons BY-NC-SA 3.0. See license file.
+"""
+from typing import Tuple
+from src.board_coordinates import BoardCoordinate
+from src.hex_meta import HexMeta
+
+
+def board_coordinate_image_location(
+    dimension: int,
+    coordinate: BoardCoordinate,
+    hex_meta: HexMeta
+) -> Tuple[int, int]:
+    """Returns the x and y offset of the given board coordinate on the board image. This allows us to draw the hex image
+    at the correct location on the board image, and will also allow us to draw other images at the correct location,
+    like the piece images.
+
+    Parameters
+    ----------
+    dimension: int
+        The dimension of the board, ie the number of hexes on a side.
+
+    coordinate: BoardCoordinate
+        The coordinate on the board to draw the image at.
+    
+    hex_meta: HexMeta
+        The metadata about the hexes.
+    """
+
+    # The BoardCoordinate has values of 0 at the center of the board, but the image coordinates have values of 0 at the
+    # bottom left of the board. So we need to shift the coordinates according to the dimension of the board.
+    col: int = coordinate.x + (2 * (dimension - 1))
+    row: int = coordinate.y + dimension - 1
+
+    # The board coordinates go from bottom to top, but the image coordinates go from top to bottom. So we need
+    # to flip the row coordinate.
+    return (
+        col * hex_meta.horizontal_step,
+        (2 * (dimension - 1) - row) * hex_meta.vertical_step
+    )
